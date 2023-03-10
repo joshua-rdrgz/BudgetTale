81a564d367dd40d627c309941ffd1a5d93e2376c
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-03-09 23:29:34 -0600

    Update

--- server/src/errors/catchAsync.ts
+++ server/src/errors/catchAsync.ts
@@ -1,9 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
-import { UserDoc } from '@models/userModel';
-
-interface IRequest extends Request {
-  user?: UserDoc;
-}
+import { Response, NextFunction } from 'express';
+import { IRequest, MiddlewareFunction } from '@types';
 
 type RouteFunction = (
   req: IRequest,
@@ -11,14 +7,8 @@ type RouteFunction = (
   next: NextFunction
 ) => Promise<void>;
 
-type ReturnedFunction = (
-  req: IRequest,
-  res: Response,
-  next: NextFunction
-) => void;
-
 export default function (fn: RouteFunction) {
   return function (req, res, next) {
     fn(req, res, next).catch(next);
-  } as ReturnedFunction;
+  } as MiddlewareFunction;
 }
