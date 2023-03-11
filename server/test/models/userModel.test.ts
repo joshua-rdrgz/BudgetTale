2f96f8d14660a174eed6a697a757e8ec1a2db822
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-03-10 21:15:18 -0600

    Update authRoute Tests, move helper fn to correct file.

--- server/test/models/userModel.test.ts
+++ server/test/models/userModel.test.ts
@@ -1,10 +1,28 @@
+// import { assert } from 'chai';
 import { HydratedDocument } from 'mongoose';
 import { userErrors } from '@errorMessages';
 import { dropTestDB } from 'test/setup.test';
 import { validateMissingOrIncorrectProperty } from './testModelUtils';
 import User, { IUser } from '@models/userModel';
+// import { generateObject } from 'test/testUtils';
 
 type UserDocument = HydratedDocument<IUser>;
+
+// const testRequirementOfUserProperties = async (
+//   propertyName: 'name' | 'email' | 'password' | 'passwordConfirm' | 'months',
+//   errorName: string,
+//   testApp: TestApp
+// ) => {
+//   const userObj = generateObject('users');
+//   delete userObj[propertyName];
+
+//   const res = await testApp.post('/api/v1/auth/createUser').send(userObj);
+//   const errorObj = JSON.parse(res.text);
+
+//   assert.deepEqual(res.status, 400);
+//   assert.deepEqual(errorObj.status, 'fail');
+//   assert.deepEqual(errorObj.message, `Invalid input data: ${errorName}`);
+// };
 
 describe('User Model', () => {
   afterEach(async () => await dropTestDB<IUser>(User, 'User'));
--- server/test/routes/authRoutes.test.ts
+++ server/test/routes/authRoutes.test.ts
@@ -1,29 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { assert } from 'chai';
 import supertest, { SuperTest } from 'supertest';
-import User, { IUser } from '@models/userModel';
 import app from 'src/app';
+import User, { IUser } from '@models/userModel';
 import { dropTestDB } from 'test/setup.test';
 import { generateObject } from 'test/testUtils';
-import { userErrors } from '@errorMessages';
 
 type TestApp = SuperTest<supertest.Test>;
-
-const testRequirementOfUserProperties = async (
-  propertyName: 'name' | 'email' | 'password' | 'passwordConfirm' | 'months',
-  errorName: string,
-  testApp: TestApp
-) => {
-  const userObj = generateObject('users');
-  delete userObj[propertyName];
-
-  const res = await testApp.post('/api/v1/createUser').send(userObj);
-  const errorObj = JSON.parse(res.text);
-
-  assert.deepEqual(res.status, 400);
-  assert.deepEqual(errorObj.status, 'fail');
-  assert.deepEqual(errorObj.message, `Invalid input data: ${errorName}`);
-};
 
 describe('Auth Routes', () => {
   let testApp: TestApp;
@@ -32,11 +15,11 @@ describe('Auth Routes', () => {
   });
   afterEach(() => dropTestDB<IUser>(User, 'User'));
 
-  describe('api/v1/createUser', () => {
+  describe('api/v1/auth/createUser', () => {
     it('POST - should create a new user', async () => {
       try {
-        const userObj = generateObject('users');
-        const res = await testApp.post('/api/v1/createUser').send(userObj);
+        const userObj = generateObject('users') as IUser;
+        const res = await testApp.post('/api/v1/auth/createUser').send(userObj);
         const resData = res.body.data.user;
         const data = await User.findById(resData._id);
 
@@ -47,35 +30,15 @@ describe('Auth Routes', () => {
         console.log(err);
       }
     });
-
-    it('POST - should fail if incorrect object given', async () => {
-      await testRequirementOfUserProperties('name', userErrors.name, testApp);
-      await testRequirementOfUserProperties('email', userErrors.email, testApp);
-      await testRequirementOfUserProperties(
-        'password',
-        userErrors.password,
-        testApp
-      );
-      await testRequirementOfUserProperties(
-        'passwordConfirm',
-        userErrors.passwordConfirm,
-        testApp
-      );
-      await testRequirementOfUserProperties(
-        'months',
-        userErrors.mustContain1Month,
-        testApp
-      );
-    });
   });
 
-  describe('/api/v1/loginUser', () => {
+  describe('/api/v1/auth/loginUser', () => {
     it('POST - should give successful JWT token when given accurate info', async () => {
       const createUserResponse = await testApp
-        .post('/api/v1/createUser')
+        .post('/api/v1/auth/createUser')
         .send(generateObject('users'));
       const loginUserResponse = await testApp
-        .post('/api/v1/loginUser')
+        .post('/api/v1/auth/loginUser')
         .send({ email: 'testing@gmail.com', password: 'testpassword' });
       
       const createUserData = createUserResponse.body.data.user;
